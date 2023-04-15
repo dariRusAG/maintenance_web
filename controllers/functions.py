e56d957a114c7b14c1d8faa132387d5400e60f8b
dariRusAG <agapova.dr@students.dvfu.ru>
2023-04-15 20:38:08 +1000

    .

---
+++ controllers/functions.py
@@ -0,0 +1,25 @@
+from flask import request, session
+from models.admin_profile_model import *
+
+
+def event(conn, df_location, value_1, value_2):
+    location_name = df_location.loc[int(request.values.get(value_1)) - 1][0]
+    venue_name = str(request.values.get(value_2))
+    add_new_venue(conn, venue_name)
+    venue_id = int(get_venue_id(conn, venue_name))
+    add_new_location(conn, venue_id, location_name)
+    get_location_id(conn, venue_id, location_name)
+
+    return venue_id, location_name
+
+
+def init_session():
+    session['types'] = []
+    session['themes'] = []
+    session['organizers'] = []
+    session['locations'] = []
+    session['status'] = []
+    session['start_date'] = "0000-00-00"
+    session['end_date'] = "9999-99-99"
+    session['start_time'] = "00:00"
+    session['end_time'] = "23:59"
